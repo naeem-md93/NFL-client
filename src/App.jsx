1e13abfecb606ba570501c4b716cc9816a60fc2c
Naeem Mohammadi <naeem.mohammadi1993@gmail.com>
2025-10-11 22:50:25 +0330

    connecting client to the server on the railway

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import TryOnComponent from "./components/TryOn/TryOn.jsx";
 import {fetchData} from "./components/utils.js";
 
 
-const SERVER_URL = import.meta.env.VITE_SERVER_URL;
+const SERVER_URL = "https://nfl-server-dev.up.railway.app:8000";
 const IMAGES_URL = `${SERVER_URL}/api/closet/images/`;
 const ITEMS_URL = `${SERVER_URL}/api/closet/items/`;
 const RECOMMENDATION_URL = `${SERVER_URL}/api/recommendation/`;
